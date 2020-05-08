61d2d6aa9ca219fa038402ebd787e8ff7473e54c
allan-viveiros <viveirosallan@gmail.com>
2020-05-07 22:56:59 -0400

    Updates

---
+++ precache-manifest.03354207c74c46c2572ab1f9e1968b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ac32ad72098f778bc3ab510ce82b97",
+    "url": "/list-react/index.html"
+  },
+  {
+    "revision": "ff5f7b9140307a8c5a89",
+    "url": "/list-react/static/css/main.519cc787.chunk.css"
+  },
+  {
+    "revision": "bd7ff498c226e5148870",
+    "url": "/list-react/static/js/2.903c52be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list-react/static/js/2.903c52be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5f7b9140307a8c5a89",
+    "url": "/list-react/static/js/main.210727c1.chunk.js"
+  },
+  {
+    "revision": "d8863ebe8f35b2c1d92f",
+    "url": "/list-react/static/js/runtime-main.7fb75f1c.js"
+  }
+]);
